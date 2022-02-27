4868ee694bf15e6f4f97f26f5566790ee620fc51
Jeremy <62273120+jeremy776@users.noreply.github.com>
2022-02-27 09:32:37 +0700

    Create Client.ts

---
+++ src/structures/Client.ts
@@ -0,0 +1,22 @@
+import { SapphireClient, SapphireClientOptions } from '@sapphire/framework';
+import { Intents } from 'discord.js';
+import { join } from 'path';
+import { Prefix } from '../config';
+import "@sapphire/plugin-api/register";
+
+export class Client extends SapphireClient {
+	public constructor(clientOptons?: SapphireClientOptions) {
+		super({
+			allowedMentions: {
+				users: [],
+				repliedUser: false
+			},
+			defaultPrefix: Prefix,
+			baseUserDirectory: join(__dirname, '..'),
+			caseInsensitiveCommands: true,
+			caseInsensitivePrefixes: true,
+			intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+			...clientOptons
+		});
+	}
+}
