20789b5dd67bff8f2b751fbbf916b8c6daa1566e
jeremy776 <jeremy.kusuma2018@gmail.com>
2022-02-28 12:32:30 +0700

    k

--- src/commands/Utility/HelpCommand.ts
+++ src/commands/Utility/HelpCommand.ts
@@ -2,8 +2,15 @@ import { CommandOptions, Command, Args } from '@sapphire/framework';
 import { MessageEmbed, Message } from 'discord.js';
 import { ApplyOptions } from '@sapphire/decorators';
 
+interface ICategory {
+  utility: string,
+  economy: string
+}
+
 @ApplyOptions<CommandOptions>({
 	name: 'help',
+	detailedDescription: "help [command]",
+	aliases: ["help", "h"],
 	description: 'show command list',
 	requiredClientPermissions: ['SEND_MESSAGES', 'EMBED_LINKS']
 })
@@ -16,17 +23,28 @@ export class HelpCommand extends Command {
 			if(!command) return;
 			const emb = new MessageEmbed()
 			.setColor('GREEN')
-			.addField("Description", `${command.description ? command.description : 'No Description'}`)
+			.setFooter({ text: `<> = required | [] = optional`})
+			.setAuthor({ name: `${command.name} Info` })
+			.addField("Description", `<:reply_1:947503681719382066> ${command.description ? command.description : 'No Description'}`)
+			.addField("Aliases", `<:reply_1:947503681719382066> \`${command.aliases.join(", ")}\``)
+			.addField("Usage", `<:reply_1:947503681719382066> \`${this.container.client.fetchPrefix(message)}${command.detailedDescription}\``)
 			return message.channel.send({ embeds: [emb] });
 		}
 		
 		const categories = [...new Set(this.container.stores.get('commands').map(x => x.fullCategory[x.fullCategory.length - 1]))];
+		const categoryEmoji: ICategory = {
+		  "utility": "<:globe:947510763071881216>",
+		  "economy": "<:coin:947526776077287465>"
+		}
 		const embed = new MessageEmbed()
+		.setDescription(`・My global prefix is \`${this.container.client.options.defaultPrefix}\`.\n<:reply_2:947503649125457980>・My prefix on this server is \`${this.container.client.fetchPrefix(message)}\`.\n<:reply_1:947503681719382066>・Type \`${this.container.client.fetchPrefix(message)}help [command]\` for more info command`)
+		.setAuthor({ name: `${this.container.client.user?.username}`, iconURL: `${this.container.client.user?.displayAvatarURL()}` })
 		.setColor('GREEN');
 		for(let category of categories) {
 			const commands = this.container.stores.get('commands').filter(x => x.category == category);
+			//let emot: 
 			embed.fields.push({
-				name: `${(category as string)}`,
+				name: `${categoryEmoji[category.toLowerCase() as keyof typeof emoji]} ・ ${(category as string)}`,
 				value: commands.map(x => `\`${x.name}\``).join(', '),
 				inline: false
 			})
